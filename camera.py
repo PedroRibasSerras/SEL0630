985df5bb58af636270dadc0e406c95ad6e6b7375
sel <email@sel>
2022-11-23 15:39:01 -0300

    Video de 5 segundos

--- camera.py
+++ camera.py
@@ -5,11 +5,18 @@ camera = PiCamera()
 camera.rotation = 180
 camera.hflip = True
 
+#Fotos
+#camera.start_preview(alpha = 200)
+#for i in range (5):
+#    sleep(2)
+#    camera.capture('./img_teste{i}.jpg'.format(i = i))
+#
+#camera.stop_preview()
 
-camera.start_preview(alpha = 200)
-for i in range (5):
-    sleep(2)
-    camera.capture('./img_teste{i}.jpg'.format(i = i))
 
+#Video
+camera.start_preview()
+camera.start_recording('./video_teste.h264')
+sleep(5)
+camera.stop_recording()
 camera.stop_preview()
-
