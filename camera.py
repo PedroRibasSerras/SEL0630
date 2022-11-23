2359f32a51adf0114eff9f859824afc21c52ea77
sel <email@sel>
2022-11-23 15:36:10 -0300

    Camera com flip e varias fotos

--- camera.py
+++ camera.py
@@ -3,8 +3,13 @@ from time import sleep
 
 camera = PiCamera()
 camera.rotation = 180
+camera.hflip = True
+
 
 camera.start_preview(alpha = 200)
-sleep(10)
+for i in range (5):
+    sleep(2)
+    camera.capture('./img_teste{i}.jpg'.format(i = i))
+
 camera.stop_preview()
 
