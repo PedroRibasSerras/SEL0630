43b423ad148e2c77f105a991a68a225a2d2becbc
sel <email@sel>
2022-11-23 15:51:17 -0300

    Mudando nome da foto do grupo e do video

--- camera.py
+++ camera.py
@@ -9,13 +9,13 @@ camera.hflip = True
 camera.start_preview(alpha = 200)
 camera.annotate_text = "Grupo master blaster Boy, Drinker e Mussattinho\n11234328, 11299978 e 11234245"
 sleep(10)
-camera.capture('./img_teste.jpg')
+camera.capture('./img_grupo.jpg')
 camera.stop_preview()
 
 
 #Video
 camera.start_preview()
-camera.start_recording('./video_teste.h264')
+camera.start_recording('./video_grupo.h264')
 sleep(5)
 camera.stop_recording()
 camera.stop_preview()
